3974a306491716fe484d0751bc6ebedfa655df6c
tomwyr <tomwyr@hotmail.com>
2024-07-27 16:56:52 +0200

    Read riot api key from env

--- Sources/App/Common/DI.swift
+++ Sources/App/Common/DI.swift
@@ -1,8 +1,10 @@
+import Vapor
+
 struct DI {
     static var rotationService: RotationService {
         RotationService(
             riotApiClient: RiotApiClient(
-                apiKey: "RGAPI-2faef0c2-c00a-417d-bd87-6dfdb9183629"
+                apiKey: Environment.get("RIOT_API_KEY")!
             )
         )
     }
