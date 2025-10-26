6548c422bfa2c8fc4a5f173cba5e6aca5adcdb0c
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-10-26 19:50:10 +0100

    Increase postgres startup timeout

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -12,7 +12,11 @@ func configure(_ app: Application, _ deps: Dependencies) async throws {
 }
 
 private func database(_ app: Application, _ deps: Dependencies) throws {
-  try app.databases.use(.postgres(url: deps.appConfig.databaseUrl), as: .psql)
+  try app.databases.use(
+    .postgres(
+      url: deps.appConfig.databaseUrl,
+      connectionPoolTimeout: .seconds(15),
+    ), as: .psql)
   app.migrations.addAppMigrations()
 }
 
